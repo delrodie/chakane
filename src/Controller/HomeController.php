ecb6d6943d19e49682f6977f94adfdc2bd2a2626
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-01-16 01:31:09 +0000

    Sitemap.xml Initialisation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,5 +13,42 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('frontend/home.html.twig');
+    }
+
+    #[Route('/sitemap.xml', name: 'app_sitemap_xml', defaults: ["_format" => 'xml'])]
+    public function sitemap(Request $request): Response
+    {
+        $hostname = $request->getSchemeAndHttpHost(); // Recuperation du nom de l'hôte depuis l'url
+        $urls = []; // Initialisation du tableau pour lister les urls
+
+        $urls[] =['loc' => $this->generateUrl('app_home')]; // Generation des urls statics
+
+        // Urls dynamiques
+        /*
+         * $presentations = $frPresentationRepository->findAll();
+         *
+         * foreach ($presentations as $presentation){
+                $images = [
+                    'loc' => '/uploads/presentation/'.$presentation->getMedia(),
+                    'title' => $presentation->getTitre()
+                ];
+                $urls[] = [
+                    'loc' => $this->generateUrl('app_frontend_presentation',['_locale'=>$loc, 'slug'=>$presentation->getSlug()]),
+                    'lastmod' => $presentation->getCreatedAt()->format('Y-m-d'),
+                    'image' => $images
+                ];
+            }
+         */
+
+        $response =new Response(
+            $this->renderView('frontend/sitemap.html.twig',[
+                'urls' => $urls,
+                'hostname' => $hostname
+            ]), 200
+        ); //dd($response);
+
+        $response->headers->add(['Content-Type' => 'text/xml']);
+
+        return $response;
     }
 }
