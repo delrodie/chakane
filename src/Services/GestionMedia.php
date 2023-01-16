a70b4bf931a2840ed00e833225f844b7856e4b3f
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-01-16 01:43:15 +0000

    Gestion media

---
+++ src/Services/GestionMedia.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+class GestionMedia
+{
+    private $mediaSlide;
+    public function __construct(
+        $slideDirectory
+    )
+    {
+        $this->mediaSlide = $slideDirectory;
+    }
+
+
+    /**
+     * @param UploadedFile $file
+     * @param $media
+     * @return string
+     */
+    public function upload(UploadedFile $file, $media = null): string
+    {
+        // Initialisation du slug
+        $slugify = new AsciiSlugger();
+
+        $originalFileName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $slugify->slug($originalFileName);
+        $newFilename = $safeFilename.'-'.Time().'.'.$file->guessExtension();
+
+        // Deplacement du fichier dans le repertoire dedié
+        try {
+            if ($media === 'slide') $file->move($this->mediaSlide, $newFilename);
+            else $file->move($this->mediaSlide, $newFilename);
+        }catch (FileException $e){
+
+        }
+
+        return $newFilename;
+    }
+
+    /**
+     * Suppression de l'ancien media sur le server
+     *
+     * @param $ancienMedia
+     * @param null $media
+     * @return bool
+     */
+    public function removeUpload($ancienMedia, $media = null): bool
+    {
+        if ($media === 'slide') unlink($this->mediaSlide.'/'.$ancienMedia);
+        else return false;
+
+        return true;
+    }
+}
